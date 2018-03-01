6975482680c7d168f1f5f34e874d2d5b9c933afc
Roman Lyapin <lyapin.rk@gmail.com>
2018-03-01 20:39:36 +0100

    Added endtime optimal paths

--- hashcode_2018/ride_order.py
+++ hashcode_2018/ride_order.py
@@ -10,6 +10,9 @@ def random_order(rides):
 def ontime_adjacency_check(first_ride, second_ride):
 	return first_ride[2][0] + first_ride[3] + dist(first_ride[1], second_ride[0]) <= second_ride[2][0]
 
+def lasttime_adjacency_check(first_ride, second_ride):
+	return first_ride[2][1] + first_ride[3] + dist(first_ride[1], second_ride[0]) <= second_ride[2][1]
+
 def optimal_ontime_rides(rides, B):
 	# Rides is an array of ride tuples
 	# A single ride looks like 
@@ -34,6 +37,53 @@ def optimal_ontime_rides(rides, B):
 		for parent in parent_rides[ride]:
 			if optimal_scores[parent][0] is not None:
 				candidate_ride_score = optimal_scores[parent][0] + B + ride[3]
+				if candidate_ride_score > optimal_ride_score:
+					optimal_ride_score = candidate_ride_score
+					optimal_parent = parent
+		optimal_scores[ride] = (optimal_ride_score, optimal_parent)
+
+	# Reconstructing optimal path
+	optimal_end = max(optimal_scores, key=lambda x: optimal_scores[x][0])
+	final_score = optimal_scores[optimal_end][0]
+
+	optimal_path = [optimal_end]
+	next_step = optimal_scores[optimal_path[-1]][1]
+	while next_step is not None:
+		optimal_path.append(next_step)
+		next_step = optimal_scores[optimal_path[-1]][1]
+
+	# Getting rid of zero entry point and reversing the path
+	optimal_path = optimal_path[:-1]
+	optimal_path.reverse()
+	for ride in rides[1:]:
+		if ride not in optimal_path:
+			optimal_path.append(ride)
+	return optimal_path, final_score
+
+def optimal_lasttime_rides(rides, B):
+	# Rides is an array of ride tuples
+	# A single ride looks like 
+	# ((0, 0), (1, 3), (2, 9), 4)
+	# (start, end, (min_start_time, max_end_time), dist)
+	rides.sort(key=lambda x: x[2][0])
+
+	rides = [((0, 0), (0, 0), (0, 0), 0)] + rides
+
+	parent_rides = {}
+	for i in range(1, len(rides)):
+		parent_rides[rides[i]] = []
+		for j in range(0, i):
+			if lasttime_adjacency_check(rides[j], rides[i]):
+				parent_rides[rides[i]].append(rides[j])
+
+	# Showing optimal scores and the associated parents
+	optimal_scores = {rides[0] : (0, None)}
+	for ride in rides[1:]:
+		optimal_ride_score = None
+		optimal_parent = None
+		for parent in parent_rides[ride]:
+			if optimal_scores[parent][0] is not None:
+				candidate_ride_score = optimal_scores[parent][0] + ride[3]
 				if candidate_ride_score > optimal_ride_score:
 					optimal_ride_score = candidate_ride_score
 					optimal_parent = parent
